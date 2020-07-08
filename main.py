f4b52eb78944e274a819b99ab28a4f6de77316e4
dima <dimich3d@ya.ru>
2020-07-08 23:26:05 +0300

    test run

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from os import environ
 from pprint import pprint
 
 from flask import Flask
+from habitica_utils import create_habitica_auth_headers, create_habitica_task, complete_habitica_task
 from todoist.api import TodoistAPI
 from dotenv import load_dotenv
 from flask import request
@@ -28,7 +29,14 @@ def my_sync():
 @app.route('/todoist_item_completed', methods=['POST'])
 def todoist_item_completed():
     request_data = request.get_json()
-    return request_data, 200
+    task_content = request_data["content"]
+    print("Got task from Todoist", task_content)
+    habitica_auth_headers = create_habitica_auth_headers()
+    created_task_id = create_habitica_task(habitica_auth_headers, task_content)
+    print("Created task in Habitica", created_task_id)
+    is_completed = complete_habitica_task(habitica_auth_headers, created_task_id)
+    print("Task completion status", is_completed)
+    return "OK", 200
 
 
 if __name__ == '__main__':
